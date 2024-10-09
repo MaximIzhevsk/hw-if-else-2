f67a6609ce17f378e904f8ddb64c541005229ef9
grafe <maximizhevsk@gmail.com>
2024-10-09 17:46:36 +0400

    задача 3 выполнена

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,8 @@ public class Main {
         System.out.println();
 
         //задача 2
+        //тут хотел обойтись одним print и без вложенных if-ов, но возникла проблема с областью
+        //видимости переменной message, знаний пока не хватило, поэтому получилась абракадабра
 
         int clientDeviceYear = 2015;
         String messageStart = "Установите ";
@@ -41,6 +43,21 @@ public class Main {
             System.out.println("Введены неверные данные");
         }
         System.out.println();
+
+        //задача 3
+
+        int year = 2021;
+
+        boolean isLeapYear = (year >= 1584 && year % 4 == 0 && year % 100 != 0) || (year >= 1584 && year % 400 == 0);
+        if (isLeapYear) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+        System.out.println();
+
+
+
 
 
     }
