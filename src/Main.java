9768b7ba0913eaa85d4014132ff0cf18793c5269
grafe <maximizhevsk@gmail.com>
2024-10-09 17:17:17 +0400

    задача 2 выполнена

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,34 @@ public class Main {
             default:
                 System.out.println("Неправильно указана операционная система");
         }
+        System.out.println();
+
+        //задача 2
+
+        int clientDeviceYear = 2015;
+        String messageStart = "Установите ";
+        String messageLight = "облегченную ";
+        String messageEnd = "версию приложения для ";
+        String OS = clientOS == 0? "iOS " : "Android ";
+        String messageLink = "по ссылке";
+
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            String message = messageStart + messageLight + messageEnd + OS + messageLink;
+            System.out.println(message);
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            String message = messageStart + messageEnd + OS + messageLink;
+            System.out.println(message);
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            String message = messageStart + messageLight + messageEnd + OS + messageLink;
+            System.out.println(message);
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            String message = messageStart + messageEnd + OS + messageLink;
+            System.out.println(message);
+        } else {
+            System.out.println("Введены неверные данные");
+        }
+        System.out.println();
+
 
     }
 }
