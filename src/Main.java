4b906709893a8f79aa03f899a7c7448f892eac69
grafe <maximizhevsk@gmail.com>
2024-10-09 16:39:56 +0400

    задача 1 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //задача 1
+
+        int clientOS = 1;
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println("Неправильно указана операционная система");
+        }
+
     }
 }
